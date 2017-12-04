108bf51e9f29ca9434321d723d03845d849a146f
Mohammad AL-Najar <32513012+malnajar@users.noreply.github.com>
2017-12-04 20:45:43 +0100

    Update 2.tasks.js

--- week1/2.tasks.js
+++ week1/2.tasks.js
@@ -45,24 +45,44 @@ let tuesday = [
 
 let tasks = monday.concat(tuesday);
 
-// convert duration to hours
+// you did not answer the first question which is :
+// 1- Collect two days' worth of tasks.
+
+
+// Request #1
+
+let worthOfTasks = 0
+
+tasks.forEach(collect => worthOfTasks += collect.duration)
+
+
+
+
 let durationInHours = tasks.map(
     task => task.duration / 60
 );
 
-// remove tasks 2hrs or longer
-durationInHours = durationInHours.filter(
-    duration => duration < 2
+// when you use filter or map the result should be a new array
+// in your below answer u have modify durationInHours by using filter which is wrong
+
+//your answer : durationInHours = durationInHours.filter(
+//    duration => duration < 2
+//);
+
+lessThenTwoHoursDuration = durationInHours.filter(duration =>
+    duration < 2
 );
 
-// calculate pay by duration
+
+
 const ratePerHour = 35;
 
+// in this question u should count all the hours not only 
+// your answer now is right because i have changed your answer 
 let payPerTask = durationInHours.map(
     duration => duration * ratePerHour
 );
 
-// calculate total pay
 let totalPay = 0;
 
 payPerTask.forEach(
@@ -71,6 +91,6 @@ payPerTask.forEach(
 
 totalPay = totalPay.toFixed(2);
 
-console.log(`Maartje's total pay is: €${totalPay}`);    // €40.83
+console.log(`Maartje's total pay is: €${totalPay}`);    // is not = €40.83
 
 
